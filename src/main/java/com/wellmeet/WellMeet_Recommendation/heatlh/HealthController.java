c319b1a618d03e7486a29dca00a885b862b57969
ezcolin2 <growth_s@naver.com>
2025-07-10 23:25:40 +0900

    feat: self hosted runner로 자동 배포 파이프라인 구현

---
+++ src/main/java/com/wellmeet/WellMeet_Recommendation/heatlh/HealthController.java
@@ -0,0 +1,12 @@
+package com.wellmeet.WellMeet_Recommendation.heatlh;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String hello(){
+        return "hello world!";
+    }
+}
